45bfa9ec26245922dae46e245d460d631118636c
im-parsa <parsa.firoozi55@gmail.com>
2022-06-22 21:14:02 +0430

    feat(routes/search): Search Train and Hotel

--- src/routes/search/train/train.component.tsx
+++ src/routes/search/train/train.component.tsx
@@ -10,11 +10,68 @@ const SearchTrain = () =>
     return (
         <main>
             <Navbar />
+
             <section className={styles.trainHeader}>
                 <div className='container'>
                     <div className={styles.trainHeaderBox}>
                         <i/>
                         <i/>
+                    </div>
+                </div>
+            </section>
+
+            <section className={styles.train}>
+                <div className='container'>
+                    <div className={styles.trainContent}>
+                        <div className={styles.trainContentFilter}>
+                            <div className={styles.trainContentFilterHeader}>
+                                <div>
+                                    <h4>نمایش 2 پرواز از 13 پرواز</h4>
+                                    <span>لغو فیلترها</span>
+                                </div>
+
+                                <ul>
+                                    <li>کلاس پروازی: فرست کلاس</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={styles.trainContentBoxes}>
+                            <div className={styles.trainContentBoxesHeader}>
+                                <h4>حدود 16 مورد قطار تهران به رشت</h4>
+                                <span>مرتب سازی بر اساس</span>
+                            </div>
+
+                            <div className={styles.trainContentBoxesDays}>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                                <div className={styles.trainContentBoxesDaysBox}>
+                                    <p>یکشنبه 5 دی</p>
+                                    <span>12/589/000 ریال</span>
+                                </div>
+                            </div>
+
+                            <div className={styles.trainContentBoxesTicket}>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
